97a1f12d41dbea9de06a10df3ef0b0643d71ce36
King Belieeve <dapomichaels@gmail.com>
2019-10-22 11:36:19 +0100

    added sensible dummy text

--- src/views/highlight.vue
+++ src/views/highlight.vue
@@ -1,30 +1,50 @@
 <template>
 	<div class="texts">
 		<div>
-			<div class="menu" :style="{left: `${x}px`, top: `${y}px`}">
-				<input class="link" placeholder="Enter link" type="text">
-				<a @click.prevent="" href="">X</a>
+			<div class="menu z-20" v-show="showLink"  :style="{left: `${x}px`, top: `${y}px`} ">
+				<span>
+					<input onblur="event.preventDefault()" class="link" autofocus placeholder="Enter a link" type="text">
+					<a class="close-link" 
+						@click.prevent="toggleLinkMenu" href>X</a>
+					
+				</span>
 			</div>
-			<!-- <div class="menu" v-show="showMenu" :style="{left: `${x}px`, top: `${y}px`}">
-				<span class="item-icons">
-					<i class="fa fa-bold item-icon-double"></i>
-					<i class="fa fa-italic"></i>
-				</span>
-				<span class="item-icons">
-					<i class="fa fa-heading item-icon-double"></i>
-					<small><i class="fa fa-heading"></i></small>
-				</span>
-				<span class="item-icons">
-					<i class="fa fa-quote-right"></i>
-				</span>
-				<span class="item-icons">
-					<i class="fa fa-list-ul item-icon-double"></i>
+			<div onmousedown="event.preventDefault()" class="menu" v-show="showMenu" :style="{left: `${x}px`, top: `${y}px`}">
+				<div class="item-icons">
+					<span @click="handleCommand('bold')">
+						<i class="fa fa-bold item-icon-double"></i>						
+					</span>
+					<span @click="handleCommand('italic')">
+						<i class="fa fa-italic"></i>
+					</span>
+				</div>
+				<div class="item-icons">
+					<span @click="handleCommand('h2')">
+						<i @click="h2" class="fa fa-heading item-icon-double"></i>
+					</span>
+					<span @click="handleCommand('h3')">
+						<small><i class="fa fa-heading"></i></small>
+					</span>
+				</div>
+				<div class="item-icons">
+					<span @click="handleCommand('quote')">
+						<i class="fa fa-quote-right"></i>
+ 					</span>
+				</div>
+				<div class="item-icons">
+					<span>
+					<i @click="insertUnorderedList" class="fa fa-list-ul item-icon-double"></i>
+						
+					</span>
+					<span>
+						
 					<i class="fa fa-list-ol"></i>
-				</span>
-				<span class="item-icons"><i class="fa fa-link"></i></span>
-			</div> -->
-			<div contenteditable="true">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod temporibus tenetur cumque culpa odio laudantium doloremque! Perspiciatis dolore nesciunt cupiditate ullam ducimus et cum, nisi voluptas veritatis obcaecati debitis, explicabo provident, eum iste eius. Quisquam sapiente, amet, expedita facere, vitae voluptatibus recusandae a ex voluptate accusantium molestiae! Voluptatibus, nobis, eos! Quae, asperiores! Similique ullam blanditiis qui ut voluptates doloremque, quo nemo reiciendis incidunt, repellendus, fugit id distinctio delectus nisi dolorum neque! Est quibusdam distinctio optio dicta ipsam consequatur ratione delectus sit vel quis unde impedit minus modi omnis iure ad voluptates, molestiae! Maxime ipsa deserunt fugiat qui veritatis aliquid temporibus at, nostrum itaque architecto unde explicabo similique doloribus vel sed amet vitae saepe odio nam aspernatur reprehenderit rerum ab. Fugiat quisquam soluta enim, explicabo maiores quaerat impedit provident rerum, fuga assumenda ipsam accusantium fugit possimus? Molestiae blanditiis laborum quasi amet tempora praesentium fuga sequi. Quo vero praesentium placeat, nobis perferendis iusto suscipit nihil fugit possimus obcaecati, alias corporis consectetur accusantium veniam iste quisquam fuga voluptatum culpa voluptates adipisci nemo, tempore quae. Excepturi exercitationem modi laboriosam minus quos temporibus! Cupiditate, quisquam odit labore consectetur sit, accusantium deleniti tempora nisi optio, magnam, natus! Facilis labore amet, explicabo temporibus quia possimus nemo! Sint.
+					</span>
+				</div>
+				<div @click.prevent="showLink = true;" class="item-icons"><i class="fa fa-link"></i></div>
+			</div>
+			<div id="editor" contenteditable="true">
+				{{text}}
 			</div>
 		</div>
 	</div>
@@ -37,30 +57,64 @@ export default {
 			x: 0,
 			y: 0,
 			showMenu: false,
-		}
+			showLink: false,
+			text: `My father’s family name being Pirrip, and my Christian name Philip, my infant tongue could make of both names nothing longer or more explicit than Pip. So, I called myself Pip, and came to be called Pip.
+
+				I give Pirrip as my father’s family name, on the authority of his tombstone and my sister,—Mrs. Joe Gargery, who married the blacksmith. As I never saw my father or my mother, and never saw any likeness of either of them (for their days were long before the days of photographs), my first fancies regarding what they were like were unreasonably derived from their tombstones. The shape of the letters on my father’s, gave me an odd idea that he was a square, stout, dark man, with curly black hair. From the character and turn of the inscription, “Also Georgiana Wife of the Above,” I drew a childish conclusion that my mother was freckled and sickly. To five little stone lozenges, each about a foot and a half long, which were arranged in a neat row beside their grave, and were sacred to the memory of five little brothers of mine,—who gave up trying to get a living, exceedingly early in that universal struggle,—I am indebted for a belief I religiously entertained that they had all been born on their backs with their hands in their trousers-pockets, and had never taken them out in this state of existence.
+
+				Ours was the marsh country, down by the river, within, as the river wound, twenty miles of the sea. My first most vivid and broad impression of the identity of things seems to me to have been gained on a memorable raw afternoon towards evening. At such a time I found out for certain that this bleak place overgrown with nettles was the churchyard; and that Philip Pirrip, late of this parish, and also Georgiana wife of the above, were dead and buried; and that Alexander, Bartholomew, Abraham, Tobias, and Roger, infant children of the aforesaid, were also dead and buried; and that the dark flat wilderness beyond the churchyard, intersected with dikes and mounds and gates, with scattered cattle feeding on it, was the marshes; and that the low leaden line beyond was the river; and that the distant savage lair from which the wind was rushing was the sea; and that the small bundle of shivers growing afraid of it all and beginning to cry, was Pip.`
+						}
 	},
 	computed: {
 		
 	},
 	created() {
-		window.addEventListener('mouseup', this.onMouseup)
+		document.addEventListener('selectionchange', this.selected);			
 	},
 	methods: {
-		onMouseup () {
-			this.showMenu = false;
+		toggleLinkMenu ()
+		{
+			this.showLink = false;
+		},
+		handleCommand(command) {
+			switch (command) {
+				case "bold":
+				document.execCommand('bold', false, null);
+				break;
+				case "italic":
+				document.execCommand('italic', false, null)
+			}
+		},
+		selected () {
+			this.showLink = false;
       const selection = window.getSelection();
-      // if (selection.toString() !== "")
       	const { x, y, width} = selection.getRangeAt(0).getBoundingClientRect();
-      	console.log(width);
 
       	if (width > 0) {
 	      	this.x = x + (width / 2);
 	      	this.y = y + window.scrollY - 20;
 	      	this.showMenu = true;
 	      	return;
-      	}else {
-      		this.showMenu = false;
-      	}      	
+      	}
+
+      	this.showMenu = false;
+    },
+    subscript()
+    {
+    	document.execCommand('subscript')
+    },
+    insertUnorderedList ()
+    {
+    	document.execCommand('insertUnorderedList')
+    },
+    h2() {
+    	document.execCommand('formatBlock', false, 'h2')
+    },
+    h3() {
+    	document.execCommand('formatBlock', false, 'h3');
+    },
+    blockQuote() {
+    	document.execCommand('formatBlock', false, 'blockquote');
     }
 	}
 }
@@ -70,6 +124,10 @@ export default {
 ::selection {
 	background: #252525;
 	color: #FFFFFF;
+}
+
+.z-20 {
+	z-index: 20;
 }
 .menu {  
 	height: 41px;
@@ -83,7 +141,7 @@ export default {
 	position: absolute; 
 	top: 0;  left: 0;  
 	transform: translate(-50%, -100%);
-	transition: 0.2s all;  
+	transition: 0.5s all;  
 	display: flex;  
 	justify-content: space-between;  
 	align-items: center;
@@ -98,22 +156,36 @@ export default {
 	color: #fff;
 	letter-spacing: 0;
 	padding: 5px 5px;
+	border-color: transparent;
 }
 
 .link::-webkit-input-placeholder {
-	color: #fff;
+	color: #565656;
 }
 
 .link:focus {
 	border: 0;
 }
+.close-link {
+	text-decoration: none; 
+	color: #fff; 
+	font-size: 13px; 
+	opacity: 0.45; 
+	position: absolute;
+	margin-left: -22px;
+	margin-top: 3px;
+}
 
 .item-icons {  
 	color: #FFF;  
-	cursor: pointer;
 	padding-right: 15px;
 	border-right: 1px solid #544949;
 	border-right-width: thin;
+}
+
+.item-icons span:hover {
+	cursor: pointer;
+	color: #ff3;
 }
 
 .item-icons:last-child { 
