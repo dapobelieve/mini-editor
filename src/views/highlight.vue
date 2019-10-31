5e18f394256ab5af621960fec23a0d04d2a91c52
Believe <believe@Believes-MacBook-Pro.local>
2019-10-31 11:06:48 +0100

    updates

--- src/views/highlight.vue
+++ src/views/highlight.vue
@@ -17,10 +17,10 @@
 					</span>
 				</div>
 				<div class="item-icons">
-					<span @click="handleCommand('h2')">
+					<span @click="handleCommand('h3')">
 						<i class="fa fa-heading item-icon-double"></i>
 					</span>
-					<span @click="handleCommand('h3')">
+					<span @click="handleCommand('h4')">
 						<small><i class="fa fa-heading"></i></small>
 					</span>
 				</div>
@@ -43,8 +43,7 @@
 					</span>
 				</div>
 			</div>
-			<div id="editor" contenteditable="true">
-				{{text}}
+			<div id="editor" v-html="text" contenteditable="true">
 			</div>
 		</div>
 	</div>
@@ -59,12 +58,18 @@ export default {
 			y: 0,
 			showMenu: false,
 			showLink: false,
-			text: `My father’s family name being Pirrip, and my Christian name Philip, my infant tongue could make of both names nothing longer or more explicit than Pip. So, I called myself Pip, and came to be called Pip.
+			text: `<section>
+				<p>My father’s family name being Pirrip, and my Christian name Philip, my infant tongue could make of both names nothing longer or more explicit than Pip. So, I called myself Pip, and came to be called Pip.</p>
 
-				I give Pirrip as my father’s family name, on the authority of his tombstone and my sister,—Mrs. Joe Gargery, who married the blacksmith. As I never saw my father or my mother, and never saw any likeness of either of them (for their days were long before the days of photographs), my first fancies regarding what they were like were unreasonably derived from their tombstones. The shape of the letters on my father’s, gave me an odd idea that he was a square, stout, dark man, with curly black hair. From the character and turn of the inscription, “Also Georgiana Wife of the Above,” I drew a childish conclusion that my mother was freckled and sickly. To five little stone lozenges, each about a foot and a half long, which were arranged in a neat row beside their grave, and were sacred to the memory of five little brothers of mine,—who gave up trying to get a living, exceedingly early in that universal struggle,—I am indebted for a belief I religiously entertained that they had all been born on their backs with their hands in their trousers-pockets, and had never taken them out in this state of existence.
+				<p>I give Pirrip as my father’s family name, on the authority of his tombstone and my sister,—Mrs. Joe Gargery, who married the blacksmith. As I never saw my father or my mother, and never saw any likeness of either of them (for their days were long before the days of photographs), my first fancies regarding what they were like were unreasonably derived from their tombstones. The shape of the letters on my father’s, gave me an odd idea that he was a square, stout, dark man, with curly black hair. From the character and turn of the inscription, 
+				“Also Georgiana Wife of the Above,” I drew a childish conclusion that my mother was freckled and sickly. To five little stone lozenges, each about a foot and a half long, which were arranged in a neat row beside their grave, and were sacred to the memory of five little brothers of mine,—who gave up trying to get a living, exceedingly early in that universal struggle,—I am indebted for a belief 
+				I religiously entertained that they had all been born on their backs with their hands in their trousers-pockets, and had never taken them out in this state of existence.</p>
 
-				Ours was the marsh country, down by the river, within, as the river wound, twenty miles of the sea. My first most vivid and broad impression of the identity of things seems to me to have been gained on a memorable raw afternoon towards evening. At such a time I found out for certain that this bleak place overgrown with nettles was the churchyard; and that Philip Pirrip, late of this parish, and also Georgiana wife of the above, were dead and buried; and that Alexander, Bartholomew, Abraham, Tobias, and Roger, infant children of the aforesaid, were also dead and buried; and that the dark flat wilderness beyond the churchyard, intersected with dikes and mounds and gates, with scattered cattle feeding on it, was the marshes; and that the low leaden line beyond was the river; and that the distant savage lair from which the wind was rushing was the sea; and that the small bundle of shivers growing afraid of it all and beginning to cry, was Pip.`
-						}
+				<h3>Ours was the marsh country, down by the river, within, as the river wound, twenty miles of the sea. My first most vivid and broad impression of the identity of things seems to me to have been gained on a memorable raw afternoon towards evening.
+				</h3>
+				<p> At such a time I found out for certain that this bleak place overgrown with nettles was the churchyard; and that Philip Pirrip, late of this parish, and also Georgiana wife of the above, were dead and buried; and that Alexander, Bartholomew, Abraham, Tobias, and Roger, infant children of the aforesaid, were also dead and buried; and that the dark flat wilderness beyond the churchyard, intersected with dikes and mounds and gates, with scattered cattle feeding on it, was the marshes; and that the low leaden line beyond was the river; and that the distant savage lair from which the wind was rushing was the sea; and that the small bundle of shivers growing afraid of it all and beginning to cry, was Pip.</p>
+				</section>`
+			}
 	},
 	created() {
 		document.addEventListener('selectionchange', this.selected);			
@@ -76,17 +81,70 @@ export default {
 			// this.showLink = !this.showLink;
 		},
 		handleCommand(command) {
+			// array of tags that could surround a text block
+			let blockTags = ['h3', 'p', 'h4', 'blockquote'];
+
+			let sel = document.getSelection();
+
+			let selectRange = sel.getRangeAt(0);
+			let currentParent = selectRange.commonAncestorContainer.parentNode;
+			let content = selectRange.commonAncestorContainer.data.toString();
+			let currentTagName = currentParent.nodeName.toLowerCase();
+			let parent, tagName, newTag;
+
+			console.log(currentTagName);
+			console.log('content::before')
+			console.log(content)
+
 			switch (command) {
 				case "bold":
 				document.execCommand('bold', false, null);
-				console.log(this.selection)
 				break;
 				case "italic":
-				document.execCommand('italic', false, null);
+					document.execCommand('italic', false, null);
+				break;
 				case "h3":
-					document.execCommand('heading', false, "h2");
+					
+					parent = blockTags.includes(currentTagName, 0) == true ? currentParent : currentParent.parentNode;
+
+					if (!blockTags.includes(currentTagName))
+						content = selectRange.commonAncestorContainer.parentElement.parentElement.innerHTML;
+					
+					
+					currentTagName = parent.nodeName.toLowerCase();
+
+					console.log('content::after')
+					console.log(content)
+					
+					tagName = currentTagName !== 'h3' ? 'h3' : 'p';
+
+					newTag = document.createElement(tagName);
+					newTag.innerHTML = content;
+
+					parent.parentNode.replaceChild(newTag, parent);
+
+					this.showMenu = false;
+				break;
 				case "h4":
-					// document.execCommand('formatBlock', false, "")
+
+					parent = blockTags.includes(currentTagName, 0) == true ? currentParent : currentParent.parentNode;
+
+					if (!blockTags.includes(currentTagName))
+						content = selectRange.commonAncestorContainer.parentElement.parentElement.innerHTML;
+					
+					
+					currentTagName = parent.nodeName.toLowerCase();
+
+					console.log(content)
+					
+					tagName = currentTagName !== 'h4' ? 'h4' : 'p';
+
+					newTag = document.createElement(tagName);
+					newTag.innerHTML = content;
+
+					parent.parentNode.replaceChild(newTag, parent);
+
+					this.showMenu = false;
 				break;
 				case "ul":
 					document.execCommand('insertUnorderedList', false);
@@ -95,7 +153,25 @@ export default {
 					document.execCommand('insertorderedlist', false);
 				break;
 				case "quote":
-					document.execCommand('formatBlock', false, 'blockquote');
+					parent = blockTags.includes(currentTagName, 0) == true ? currentParent : currentParent.parentNode;
+
+					if (!blockTags.includes(currentTagName))
+						content = selectRange.commonAncestorContainer.parentElement.parentElement.innerHTML;
+					
+					
+					currentTagName = parent.nodeName.toLowerCase();
+
+					console.log('content::after')
+					console.log(content)
+					
+					tagName = currentTagName !== 'blockquote' ? 'blockquote' : 'p';
+
+					newTag = document.createElement(tagName);
+					newTag.innerHTML = content;
+
+					parent.parentNode.replaceChild(newTag, parent);
+
+					this.showMenu = false;
 				break;
 			}
 		},
@@ -119,6 +195,8 @@ export default {
 </script>
 
 <style scoped>
+
+
 ::selection {
 	background: #252525;
 	color: #FFFFFF;
